7397233cd753d0b0bcd1fc5677f9ffa000c62c63
Olly Holovchenko <olly.holovchenko@sky.uk>
2023-09-24 18:57:28 +0100

    Covert the app to Vite.

--- drawmoji-vite/src/Games/Drawmoji/Drawmoji.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './Drawmoji';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- drawmoji-vite/src/Games/WhattaDraw/WhattaDraw.tsx
+++
@@ -1,5 +0,0 @@
-import { Box } from '@mui/material';
-
-export const WhattaDraw = () => {
-  return <Box>hello world</Box>;
-};
